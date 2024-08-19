d413f1c82a484f7979176ee38bd7d7fae6a507c1
buradabhargavi <burada.bhargavi@actualpixel.in>
2024-08-19 11:40:22 +0530

    login ui

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,236 @@
+import React, { useRef, useState } from 'react';
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  Link,
+  Checkbox,
+  TextField,
+  Typography,
+  FormControlLabel,
+  Button,
+  styled,
+} from '@mui/material';
+
+import { colors } from 'src/themes/colors';
+import { theme } from 'src/themes/theme';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import HomeImage from '../../assets/HomeImage.png';
+import logo from '../../assets/Actual-pixel-logo.png';
+
+const Container = styled(Box)({
+  width: '100vw',
+  height: '100vh',
+  display: 'flex',
+  position: 'relative',
+});
+
+const LeftContainer = styled(Box)({
+  width: '50%',
+  background: colors.primary.metallicViolet,
+  padding: '10% 6%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.whitespace.spacings[32],
+});
+
+const RightContainer = styled(Box)({
+  width: '50%',
+  padding: '10%',
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const ImageContainer = styled(Box)({
+  position: 'absolute',
+  top: '3rem',
+  right: '3rem',
+});
+
+const LogoImage = styled('img')({
+  width: 'auto',
+  height: 'auto',
+});
+
+const Heading = styled(Typography)({
+  color: colors.body.white,
+  fontFamily: theme.typography.fontFamily.headers,
+  wordSpacing: theme.typography.letterSpacing.small,
+});
+
+const SubHeading = styled(Typography)({
+  color: colors.body.white,
+  opacity: 0.4,
+  marginTop: '0.5rem',
+  fontFamily: theme.typography.fontFamily.body,
+});
+
+const ImageBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const HomeImageStyled = styled('img')({
+  width: '60%',
+  height: '90%',
+});
+
+const LoginHeading = styled(Typography)({
+  fontFamily: theme.typography.fontFamily.headers,
+  color: colors.headers.darkBlack,
+  fontWeight: theme.typography.fontWeights.bold,
+  marginBottom: '0.3rem',
+  wordSpacing: theme.typography.letterSpacing.small,
+});
+
+const LoginSubHeading = styled(Typography)({
+  color: colors.body.lightGrey,
+  fontFamily: theme.typography.fontFamily.body,
+  fontWeight: theme.typography.fontWeights.default,
+  marginBottom: '2.5rem',
+});
+
+const StyledLabel = styled(Typography)({
+  fontFamily: theme.typography.fontFamily.body,
+  color: colors.body.lightGrey,
+  marginBottom: theme.whitespace.spacings[2],
+});
+
+const StyledTextField = styled(TextField)({
+  marginBottom: theme.whitespace.spacings[8],
+  width: '100%',
+});
+
+const StyledLink = styled(Link)({
+  fontFamily: theme.typography.fontFamily.body,
+  color: colors.primary.electricIndigo,
+  textDecoration: 'none',
+  fontSize: theme.typography.fontSizes[10],
+});
+
+const StyledButton = styled(Button)({
+  background: colors.primary.metallicViolet,
+  fontFamily: theme.typography.fontFamily.body,
+  color: colors.body.white,
+  marginBottom: '0.5rem',
+  marginTop: '1.5rem',
+  width: '100%',
+});
+
+const StyledFormControlLabel = styled(FormControlLabel)({
+  fontFamily: theme.typography.fontFamily.body,
+  color: colors.body.lightGrey,
+  fontSize: theme.typography.fontSizes[10],
+});
+
+const FlexBox = styled(Box)({
+  marginBottom: '10px',
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const StyledBox = styled(Box)({
+  maxWidth: '350px',
+});
+
+const CenteredText = styled(Typography)({
+  textAlign: 'center',
+  fontFamily: theme.typography.fontFamily.body,
+  color: colors.body.lightGrey,
+  fontSize: theme.typography.fontSizes[10],
+});
+
 function Login() {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const userId = useRef<HTMLInputElement>(null);
+  const Password = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const enteredId = userId.current?.value;
+    const enteredPassword = Password.current?.value;
+    console.log(enteredId, enteredPassword);
+  };
+
+  const handlePasswordShow = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <Container>
+      <ImageContainer>
+        <LogoImage src={logo} alt="Logo" />
+      </ImageContainer>
+
+      <LeftContainer>
+        <Box>
+          <Heading variant="h4">Lorem ipsum dolor sit amet, cons...</Heading>
+          <SubHeading variant="body1">
+            Fusce volutpat lectus et nisl consectetur finibus. In vitae
+            scelerisque augue, in varius eros. Nunc sapien diam, euismod et
+            pretium id.
+          </SubHeading>
+        </Box>
+        <ImageBox>
+          <HomeImageStyled src={HomeImage} alt="Home" />
+        </ImageBox>
+      </LeftContainer>
+
+      <RightContainer>
+        <LoginHeading variant="h4">Login to your account.</LoginHeading>
+        <LoginSubHeading variant="body2">
+          Enter your email address and password to continue with us.
+        </LoginSubHeading>
+        <StyledBox>
+          <form onSubmit={handleSubmit}>
+            <StyledLabel variant="body2">Employee ID/Email ID</StyledLabel>
+            <StyledTextField
+              size="small"
+              placeholder="1004320"
+              inputRef={userId}
+            />
+            <StyledLabel variant="body2">Your Password</StyledLabel>
+            <StyledTextField
+              size="small"
+              placeholder="Enter your password"
+              inputRef={Password}
+              type={showPassword ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handlePasswordShow}>
+                      {showPassword ? (
+                        <VisibilityIcon />
+                      ) : (
+                        <VisibilityOffIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FlexBox>
+              <StyledFormControlLabel
+                control={<Checkbox size="small" />}
+                label="Remember me?"
+              />
+              <StyledLink>Forgot Password?</StyledLink>
+            </FlexBox>
+            <StyledButton variant="contained" type="submit">
+              Sign In Now
+            </StyledButton>
+            <CenteredText variant="body2">
+              Don’t have an account? <StyledLink>sign up now.</StyledLink>
+            </CenteredText>
+          </form>
+        </StyledBox>
+      </RightContainer>
+    </Container>
+  );
 }
 
 export default Login;
--- src/routers/allRoutes.tsx
+++ src/routers/allRoutes.tsx
@@ -2,16 +2,17 @@ import {
   createRoutesFromElements,
   createBrowserRouter,
   Route,
-} from "react-router-dom";
-import ProtectedRoutes from "src/routers/ProtectedRoutes";
-import { paths } from "src/routers/paths";
-import { CounterPage, HomePage, ErrorPage } from "src/routers/loads/lazyLoads";
+} from 'react-router-dom';
+import ProtectedRoutes from 'src/routers/ProtectedRoutes';
+import { paths } from 'src/routers/paths';
+import { CounterPage, HomePage, ErrorPage } from 'src/routers/loads/lazyLoads';
+import Login from '../pages/login/Login';
 
 const allRoutes = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route errorElement={<ErrorPage />}>
-        <Route path={`/${paths.login}`} element={<CounterPage />} />
+        <Route path={`/${paths.login}`} element={<Login />} />
 
         {/* /protected */}
         <Route element={<ProtectedRoutes />} path={paths.root}>
