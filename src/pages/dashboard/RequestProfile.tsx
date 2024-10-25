6d4df22f0be8f8af6da213f4bb552d4307cd88f8
Buradabhargavi <burada.bhargavi@actualpixel.in>
2024-10-25 16:48:31 -0700

    Request Profile UI

---
+++ src/pages/dashboard/RequestProfile.tsx
@@ -0,0 +1,76 @@
+import TopNav from 'src/components/navbar/TopNav';
+import Profile from './Profile';
+import { Box, Typography, styled } from '@mui/material';
+import { colors } from 'src/themes/colors';
+import { theme } from 'src/themes/theme';
+import Logo from '../../assets/Actual-pixel-logo.svg';
+
+const MainContainer = styled(Box)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+}));
+const HeaderContainer = styled(Box)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0 3%',
+  borderBottom: `1px solid ${colors.greys.lightGrey}`,
+  height: '100px',
+});
+
+const LogoImage = styled('img')({
+  width: '100px',
+  height: '50px',
+});
+
+const ContentBox = styled(Box)({
+  width: '80%',
+});
+
+const WelcomeContainer = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: theme.whitespace.spacings[12],
+  '& .welcome-title': {
+    fontSize: theme.typography.fontSizes[20],
+    fontFamily: theme.typography.fontFamily.main,
+    color: colors.primary.metallicViolet,
+    fontWeight: theme.typography.fontWeights.semiBold,
+  },
+  '& .welcome-message': {
+    fontSize: theme.typography.fontSizes[16],
+    fontFamily: theme.typography.fontFamily.main,
+    color: colors.body.lightGrey,
+  },
+});
+
+const StyledContainer = styled(Box)({
+  padding: '3% 10%',
+});
+
+function RequestProfile() {
+  return (
+    <MainContainer>
+      <HeaderContainer>
+        <LogoImage src={Logo} alt="Logo" />
+        <ContentBox>
+          <TopNav />
+        </ContentBox>
+      </HeaderContainer>
+      <WelcomeContainer>
+        <Typography className="welcome-title">
+          Welcome to Pixel Vault!
+        </Typography>
+        <Typography className="welcome-message">
+          Your account has been created and sent for the Admin approval.
+        </Typography>
+      </WelcomeContainer>
+      <StyledContainer>
+        <Profile />
+      </StyledContainer>
+    </MainContainer>
+  );
+}
+export default RequestProfile;
